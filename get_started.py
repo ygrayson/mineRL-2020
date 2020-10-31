3da590499539367c52f77bea375adb9a905685fe
Qianbo Yin <graysonyin717@gmail.com>
2020-10-31 21:21:47 +0800

    local commit

--- get_started.py
+++ get_started.py
@@ -9,6 +9,12 @@ obs = env.reset()
 
 # start taking actions
 done = False
+all_reward = []
+all_angles = []
 while not done:
     action = env.action_space.sample()
     obs, reward, done, _ = env.step(action)
+    all_reward.append(reward)
+    all_angles.append(obs['compassAngle'])
+
+print("Done!")
